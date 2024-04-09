54acd59fe024a1c67b98a1646c3bfa6b20198eb4
Nikita <nikita2907200303@gmail.com>
2024-04-09 18:04:49 +0300

    Initial Commit

---
+++ AezakmiGroupTestTask/AezakmiGroupTestTaskApp.swift
@@ -0,0 +1,17 @@
+//
+//  AezakmiGroupTestTaskApp.swift
+//  AezakmiGroupTestTask
+//
+//  Created by Никита Иванов on 09.04.2024.
+//
+
+import SwiftUI
+
+@main
+struct AezakmiGroupTestTaskApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
